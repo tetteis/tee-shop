d0ed63459a31ba2fdb4e89457222f8b567ac0933
Tettei Shahday-Annang <tetteis@gmail.com>
2023-04-24 20:23:16 +0000

    Implemented dark theme.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,35 @@
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import Button from "./Button";
 
 export default function Navbar(props) {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
   const cartCount = props.cart.reduce(
     (total, product) => total + product.quantity,
     0
   );
+
+  useEffect(() => {
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    if (prefersDark) {
+      setIsDarkTheme(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isDarkTheme) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [isDarkTheme]);
+
+  function handleThemeClick() {
+    setIsDarkTheme(!isDarkTheme);
+  }
 
   return (
     <nav className="navbar">
@@ -12,6 +37,12 @@ export default function Navbar(props) {
         TeeShop
       </NavLink>
       <ul>
+        <li className="nav-item">
+          <Button className="theme-switcher" onClick={handleThemeClick}>
+            {isDarkTheme ? "Dark" : "Light"}
+          </Button>
+        </li>
+
         <li className="nav-item">
           <NavLink
             end
